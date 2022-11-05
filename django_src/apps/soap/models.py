593204bc5df7bc3255d364344f9625858cf04111
Diego Sánchez <diegosandmg@gmail.com>
2022-11-05 15:00:06 -0400

    feat: soap country service

--- django_src/apps/soap/models.py
+++ django_src/apps/soap/models.py
@@ -2,23 +2,91 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
-class TaskStates(models.TextChoices):
-    TODO = "TODO", _("Por hacer")
-    DONE = "DONE", _("Hecha")
-
-class Task(models.Model):
-    """
-    Una tarea que hacemos todos los días
-    """
-    description = models.CharField(
-        _("descripcion de la tarea"),
-        max_length=250
-    )
-    states = models.TextField(
-        _("Estado de la tarea"),
-        choices=TaskStates.choices,
-        default=TaskStates.TODO,
+class Country(models.Model):
+    name = models.CharField(
+        verbose_name=_("Name"),
+        max_length=100
     )
 
-    def __str__(self):
-        return f"description={self.description} state={self.states}"
+    iso3 = models.CharField(
+        verbose_name=_("iso3 abbreviation code"),
+        max_length=3,
+        blank=True,
+    )
+
+    numeric_code = models.CharField(
+        verbose_name=_("Numeric code "),
+        max_length=100,
+        blank=True,
+    )
+
+    iso2 = models.CharField(
+        verbose_name=_("iso2 abbreviation"),
+        max_length=2,
+        blank=True,
+    )
+
+    phonecode = models.CharField(
+        verbose_name=_("Phone code "),
+        max_length=255,
+        blank=True,
+    )
+
+    capital = models.CharField(
+        max_length=255,
+        blank=True,
+    )
+
+    # Continent
+    continent=models.CharField(
+        verbose_name=_("Continent"),
+        max_length=255,
+        blank=True,
+    )
+
+    subregion=models.CharField(
+        verbose_name=_("Sub region"),
+        max_length=255,
+        blank=True,
+    )
+    def __str__(self) -> str:
+        return f"{self.continent} > {self.name}"
+
+class State(models.Model):
+
+    name = models.CharField(
+        verbose_name=_("Name"),
+        max_length=255,
+    )
+
+    iso2 = models.CharField(
+        verbose_name=_("iso2 abbreviation"),
+        max_length=255,
+        blank=True,
+    )
+    country = models.ForeignKey(
+        to="Country",
+        verbose_name=_("Country"),
+        on_delete=models.CASCADE,
+        related_name="states",
+    )
+
+    def __str__(self) -> str:
+        return f"{self.country.name} > {self.name}"
+
+class City(models.Model):
+
+    name = models.CharField(
+        verbose_name=_("Name"),
+        max_length=255,
+    )
+
+    state = models.ForeignKey(
+        to="State",
+        verbose_name=_("City"),
+        on_delete=models.CASCADE,
+        related_name="cities",
+    )
+
+    def __str__(self) -> str:
+        return f"{self.state.country.name} > {self.state.name} > {self.name}"
